029fa630ce4912fbb2ba4c1b40a2c6a68b050f89
Tokotome <mandzhukov.bg@gmail.com>
2023-12-10 19:34:35 +0200

    add palindromeNumber task resolved

---
+++ palindromeNumber.php
@@ -0,0 +1,16 @@
+<?php 
+//Given an integer x, return true if x is a  palindrome, and false otherwise.
+
+function isPalindrome($int) {
+    $$int = strval($int);
+    $array = str_split($int);
+    $arrayReverse = array_reverse($array);
+
+    if($array === $arrayReverse) {
+        return true;
+    } else {
+        return false;
+    }     
+}
+?>
+
