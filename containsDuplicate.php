73bfbd57e5dfdc43890c58d4037c882675f8c0f3
Tokotome <mandzhukov.bg@gmail.com>
2023-12-11 13:59:46 +0200

    resolve containsDuplicate issue

---
+++ containsDuplicate.php
@@ -0,0 +1,10 @@
+<?php 
+//Given an integer array nums, return true if any value appears at least twice in the array, 
+//and return false if every element is distinct.
+
+    function containsDuplicate($nums) {
+        $uniques = array_unique($nums);
+        return count($uniques) != count($nums) ? true : false;
+    }
+?>
+
