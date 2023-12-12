85bdff19e26bda52525c92d8659003c4d1f717e2
Tokotome <mandzhukov.bg@gmail.com>
2023-12-12 16:24:19 +0200

    resolve maximum product of two numbers in arr

---
+++ MaximumProductOfTwoNumbers.php
@@ -0,0 +1,12 @@
+<?php   
+
+//Given the array of integers nums, you will choose two different indices i and j of that array. 
+//Return the maximum value of (nums[i]-1)*(nums[j]-1).
+
+function maxProduct($nums) {
+    sort($nums);
+    return ($nums[count($nums)-1]-1 ) * ($nums[count($nums)-2]-1);
+}
+
+maxProduct([1,5,4,6]);
+?>
