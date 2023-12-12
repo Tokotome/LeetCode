2574cacb6289d50093cb5cbff3f74edc980b89a5
Tokotome <mandzhukov.bg@gmail.com>
2023-12-12 17:27:46 +0200

    resolve final value after operations

---
+++ finalValueOfVariableAfterOperations.php
@@ -0,0 +1,21 @@
+<?php 
+//There is a programming language with only four operations and one variable X:
+//++X and X++ increments the value of the variable X by 1.
+//--X and X-- decrements the value of the variable X by 1.
+//Initially, the value of X is 0.
+//Given an array of strings operations containing a list of operations, 
+//return the final value of X after performing all the operations.
+
+function finalValueAfterOperations($operations) {
+     $start = 0;
+     foreach ($operations as $operation) { 
+        if (str_contains($operation, '-')) {
+            $start --;
+        } else {
+            $start ++;
+        }
+     } 
+
+     return $start;
+}
+?>
