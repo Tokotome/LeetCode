bcc263fc11bb73221429e43462e8897a2e745e7d
Tokotome <mandzhukov.bg@gmail.com>
2023-12-21 16:39:21 +0200

    resolve max number of words in sentences

---
+++ maximumNumberofWordsFoundinSentences.php
@@ -0,0 +1,29 @@
+<?php 
+// https://leetcode.com/problems/maximum-number-of-words-found-in-sentences
+
+// A sentence is a list of words that are separated by a single space with no leading or trailing spaces.
+// You are given an array of strings sentences, where each sentences[i] represents a single sentence.
+// Return the maximum number of words that appear in a single sentence.
+
+class Solution {
+
+    /**
+     * @param String[] $sentences
+     * @return Integer
+     */
+    function mostWordsFound($sentences) {
+        $largestCount = 0;
+
+        foreach($sentences as $k => $v) {
+            $count = count(explode(' ',$v));
+            if($count >= $largestCount) {
+                $largestCount = $count;
+            }
+        }
+        print_r($largestCount);
+    }
+}
+
+$sol = new Solution;
+$sol->mostWordsFound(["please wait", "continue to fight", "continue to win"]);
+?>
