fc2fdb47a3084c94e4711ed227b577ff24410b35
Tokotome <mandzhukov.bg@gmail.com>
2024-04-01 10:41:00 +0300

    test commit

---
+++ mergeTwoSortedLists.php
@@ -0,0 +1,33 @@
+<?php 
+// https://leetcode.com/problems/merge-two-sorted-lists/
+
+// You are given the heads of two sorted linked lists list1 and list2.
+// Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+// Return the head of the merged linked list.
+
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val = 0, $next = null) {
+ *         $this->val = $val;
+ *         $this->next = $next;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param ListNode $list1
+     * @param ListNode $list2
+     * @return ListNode
+     */
+    function mergeTwoLists($list1, $list2) {
+        
+    }
+}
+
+$sol = new Solution;
+$sol->reverseString([1,2,4], [1,3,4] )
+?>
