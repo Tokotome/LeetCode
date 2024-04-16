8fe17d8a556288b3fdbfeb347d576028d8d27960
Tokotome <mandzhukov.bg@gmail.com>
2024-04-16 11:41:03 +0300

    change bubble sort logic

--- minimumNumberGame.php
+++ minimumNumberGame.php
@@ -23,12 +23,12 @@ class Solution {
     function numberGame($nums) {
         $n = count($nums);
         
-        for ($i = 0; $i < $n - 1; $i++) {
-            for ($j = 0; $j < $n - $i - 1; $j++) {
-                if ($nums[$j] > $nums[$j + 1]) {
-                    $temp = $nums[$j];
-                    $nums[$j] = $nums[$j + 1];
-                    $nums[$j + 1] = $temp;
+        for ($i = 0; $i < $n; $i++) {
+            for ($j = 0; $j < $n - 1; $j++) {
+                if ($nums[$j] > $nums[$j+1]) {
+                    $temp = $nums[$j+1];
+                    $nums[$j+1] = $nums[$j];
+                    $nums[$j] = $temp;
                 }
             }
         }
