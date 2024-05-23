8e75964214fdd18dce8e329a6c52481fe7c7bf88
Tokotome <mandzhukov.bg@gmail.com>
2024-05-23 12:18:06 +0300

    add php unit tests in folder and one test

---
+++ Tests/splitStringBySeparatorTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+require_once __DIR__ . '\..\Tasks\splitStringsBySeparator.php';
+
+class splitStringBySeparatorTest extends TestCase
+{
+    public function testSplitWordsBySeparator()
+    {
+        $solution = new SolutionSplitStringsBySeparator();
+
+        $words = ['$easy$', '$problem$'];
+        $separator = '$';
+        $expected = ['easy', 'problem'];
+        
+        $result = $solution->splitWordsBySeparator($words, $separator);
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testSplitWordsByDifferentSeparator()
+    {
+        $solution = new SolutionSplitStringsBySeparator();
+
+        $words = ['apple,orange', 'banana,mango'];
+        $separator = ',';
+        $expected = ['apple', 'orange', 'banana', 'mango'];
+        
+        $result = $solution->splitWordsBySeparator($words, $separator);
+
+        $this->assertEquals($expected, $result);
+    }
+
+    // You can add more tests to cover other cases
+}
+?>
